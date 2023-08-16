66dbca0687bdf373653c22b4e66f84b003358e9c
sivakumarreddy <info@joindevops.com>
2023-08-16 07:54:26 +0530

    hello world script added

--- 01-hello-world.sh
+++ 01-hello-world.sh
@@ -1,8 +1,10 @@
 #!/bin/bash
 
 echo "Hello World, I am learning DevOps with AWS"
+PERSON1=Ramesh
+PERSON2=Suresh
 
-echo "Ramesh: Hi Suresh, Good Morning"
-echo "Suresh: Good Morning Ramesh"
-echo "Ramesh: How are you Suresh"
-echo "Suresh: i am good, how are you Ramesh"
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: i am good, how are you $PERSON1"
