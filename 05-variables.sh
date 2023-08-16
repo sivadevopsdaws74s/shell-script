af8a935c844db86aa8424f2893717136372c9556
sivakumarreddy <info@joindevops.com>
2023-08-16 08:12:36 +0530

    hello world script added

--- 05-variables.sh
+++ 05-variables.sh
@@ -2,12 +2,12 @@
 
 echo "Please enter your username"
 
-read USERNAME # this is the variable where the value of the one user entered
+read -s USERNAME # this is the variable where the value of the one user entered
 
 echo "Username entered is: $USERNAME"
 
 echo "Please enter your password"
 
-read PASSWORD
+read -s PASSWORD
 
 echo "Password entered is: $PASSWORD"
