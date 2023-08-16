4cdd0057e839a11aab7033558b1e92e29cd33781
sivakumarreddy <info@joindevops.com>
2023-08-16 08:01:42 +0530

    hello world script added

---
+++ 03-variables.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+echo "Hello World, I am learning DevOps with AWS"
+PERSON1=$1 # this is the first value from command line
+PERSON2=$2 # # this is the second value from command line
+
+echo "$PERSON1: Hi $PERSON2, Good Morning"
+echo "$PERSON2: Good Morning $PERSON1"
+echo "$PERSON1: How are you $PERSON2"
+echo "$PERSON2: i am good, how are you $PERSON1"
