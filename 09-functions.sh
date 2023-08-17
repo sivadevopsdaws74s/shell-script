c42d84e358250f62951c931b3fded3de24425fd5
sivakumarreddy <info@joindevops.com>
2023-08-17 08:10:59 +0530

    added condition

--- 09-functions.sh
+++ 09-functions.sh
@@ -15,10 +15,10 @@ VALIDATE(){
     #$1 --> it will receive the argument1
     if [ $1 -ne 0 ]
     then
-        echo "$2 ... $R FAILURE $N"
+        echo -e "$2 ... $R FAILURE $N"
         exit 1
     else
-        echo "$2 ... $G SUCCESS $N"
+        echo -e "$2 ... $G SUCCESS $N"
     fi
 }
 
