81ceefd03bafad946ae890073012ba29661e1f85
sivakumarreddy <info@joindevops.com>
2023-08-18 07:29:40 +0530

    added condition

--- 12-loops.sh
+++ 12-loops.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
 
+DATE=$(date %+F)
+LOGSDIR=/home/centos/shellscript-logs
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
 USERID=$(id -u)
 R="\e[31m"
+G="\e[32m"
 N="\e[0m"
+Y="\e[33m"
 
 if [ $USERID -ne 0 ];
 then
@@ -10,10 +17,30 @@ then
     exit 1
 fi
 
+VALIDATE(){
+    if [ $1 -ne 0 ];
+    then
+        echo -e "Installing $2 ... $R FAILURE $N"
+        exit 1
+    else
+        echo -e "Installing $2 ... $G SUCCESS $N"
+    fi
+}
+
 # all args are in $@
 for i in $@
 do
-    yum install $i -y
+    yum list installed $i &>>$LOGFILE
+    if [ $? -ne 0 ]
+    then
+        echo "$i is not installed, let's install it"
+        yum install $i -y &>>$LOGFILE
+        VALIDATE $? "$i"
+    else
+        echo -e "$Y $i is already installed $N"
+    fi
+
+    #yum install $i -y
 done
 
 # improvements
