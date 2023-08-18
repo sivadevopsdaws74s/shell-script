292810a71cafd90d594b13132eabf7e6233cb582
sivakumarreddy <info@joindevops.com>
2023-08-18 07:45:49 +0530

    added condition

--- 13-delete-old-logs.sh
+++ 13-delete-old-logs.sh
@@ -11,3 +11,9 @@ LOGFILE=$LOGSDIR/$0-$DATE.log
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 
 echo "$FILES_TO_DELETE"
+
+while read line
+do
+    echo "Deleting $line" &> $LOGFILE
+    rm -rf $line
+done < $FILES_TO_DELETE
