489792db9bc8a27c770afa2cb89a98f3db6fe6e5
sivakumarreddy <info@joindevops.com>
2023-08-18 07:54:33 +0530

    added condition

--- 13-delete-old-logs.sh
+++ 13-delete-old-logs.sh
@@ -2,7 +2,7 @@
 
 APP_LOGS_DIR=/home/centos/app-logs
 
-DATE=$(date +%F)
+DATE=$(date +%F:%H:%M:%S)
 LOGSDIR=/home/centos/shellscript-logs
 # /home/centos/shellscript-logs/script-name-date.log
 SCRIPT_NAME=$0
@@ -10,7 +10,7 @@ LOGFILE=$LOGSDIR/$0-$DATE.log
 
 FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
 
-echo "$FILES_TO_DELETE"
+echo "script started executing at $DATE"
 
 while read line
 do
