f13b407d93105ef2f7a230427afac88d9e65939d
sivakumarreddy <info@joindevops.com>
2023-08-18 07:42:14 +0530

    added condition

---
+++ 13-delete-old-logs.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+APP_LOGS_DIR=/home/centos/app-logs
+
+DATE=$(date +%F)
+LOGSDIR=/home/centos/shellscript-logs
+# /home/centos/shellscript-logs/script-name-date.log
+SCRIPT_NAME=$0
+LOGFILE=$LOGSDIR/$0-$DATE.log
+
+FILES_TO_DELETE=$(find $APP_LOGS_DIR -name "*.log" -type f -mtime +14)
+
+echo "$FILES_TO_DELETE"
