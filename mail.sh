e129a04656a3c28579b38933eb4ec79eddd91739
sivakumarreddy <info@joindevops.com>
2023-08-21 07:59:39 +0530

    added script

--- mail.sh
+++ mail.sh
@@ -9,5 +9,9 @@ BODY=$3
 TEAM_NAME=$4
 ALERT_TYPE=$5
 
-echo "all args: $@"
+#echo "all args: $@"
+
+FINAL_BODY=$(sed -e 's/TEAM_NAME/DevOps Team/g' -e 's/ALERT_TYPE/High Disk Usage/g' -e "s/MESSAGE/$BODY" template.html)
+
+echo "$FINAL_BODY" | mail -s "$SUBJECT" $TO_ADDRESS
 
