b413420b48d88f6b7672983db7ceafd7f06f628d
sivakumarreddy <info@joindevops.com>
2023-08-21 08:08:37 +0530

    added script

--- mail.sh
+++ mail.sh
@@ -12,7 +12,7 @@ ALERT_TYPE=$5
 
 #echo "all args: $@"
 
-FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/" template.html)
+FINAL_BODY=$(sed -e "s/TEAM_NAME/$TEAM_NAME/g" -e "s/ALERT_TYPE/$ALERT_TYPE/g" -e "s/MESSAGE/$BODY/g" template.html)
 
 echo "$FINAL_BODY" | mail -s "$SUBJECT" "$TO_ADDRESS"
 
